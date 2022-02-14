2718765c740a07df7dddef658c52ee6aa89523e7
kimsa1984 <suna5012kim@gmail.com>
2022-02-14 19:29:12 +0900

    sssss

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
+
 var currentScrollTop = 0;
 $(document).ready(function () {
     scrollController();
     $(window).scroll(function () {
         scrollController();
     });
-
 });
 
 function scrollController() {
     currentScrollTop = $(window).scrollTop();
     if (currentScrollTop > 100) {
-        $("#nav").addClass("fixed");
-        $("#nav a").addClass("txtCh");
+        $("nav").addClass("fixed");
+        $("nav a").addClass("txtCh");
     } else {
-        $("#nav").removeClass("fixed");
-        $("#nav a").removeClass("txtCh");
+        $("nav").removeClass("fixed");
+        $("nav a").removeClass("txtCh");
     }
 }
