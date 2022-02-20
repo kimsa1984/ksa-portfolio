6024240ebc864419476e23791f70c48496614166
kimsa1984 <93236999+kimsa1984@users.noreply.github.com>
2022-02-21 05:30:53 +0900

    dd

--- js/main.js
+++ js/main.js
@@ -17,3 +17,38 @@ function scrollController() {
         $("nav a").removeClass("txtCh");
     }
 }
+
+$(function () {
+    $('.popbox1').hide();
+    $('.popbox2').hide();
+    $('.popbox3').hide();
+    $('.popbox4').hide();
+
+    $(".port_box1").click(function () {
+        $(".bottom, .popbox1").fadeIn();
+    });
+    $(".bottom").click(function () {
+        $(".bottom, .popbox1").fadeOut();
+    });
+
+    $(".port_box2").click(function () {
+        $(".bottom, .popbox2").fadeIn();
+    });
+    $(".bottom").click(function () {
+        $(".bottom, .popbox2").fadeOut();
+    });
+
+    $(".port_box3").click(function () {
+        $(".bottom, .popbox3").fadeIn();
+    });
+    $(".bottom").click(function () {
+        $(".bottom, .popbox3").fadeOut();
+    });
+
+    $(".port_box4").click(function () {
+        $(".bottom, .popbox4").fadeIn();
+    });
+    $(".bottom").click(function () {
+        $(".bottom, .popbox4").fadeOut();
+    });
+});
